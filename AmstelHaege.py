108513303f997d576085b90c1259b6972d18fb02
Michael de Jong <michael.dejong@student.uva.nl>
2020-01-07 10:30:46 +0100

    Added csv read code

--- AmstelHaege.py
+++ AmstelHaege.py
@@ -1,4 +1,13 @@
 import numpy as np
+import csv
 
-A = np.zeros([400,180])
-print(A)
+with open('./wijken/wijk_1.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, quotechar='"', quoting=csv.QUOTE_ALL,
+        skipinitialspace=True)
+    T = []
+    for row in reader:
+        T.append(row)
+A = np.zeros([160,180])
+print(T)
+# structure, bottom_left_xy, top_right_xy, type
+# water_1, "0,0", "32,180", WATER
