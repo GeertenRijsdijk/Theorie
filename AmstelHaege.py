d8619f76a58a446548999d56990aeed8cf4c82c7
Geerten Rijsdijk <geertenrijsdijk@hotmail.com>
2020-01-08 11:06:54 +0100

    find_spot function almost complete

--- AmstelHaege.py
+++ AmstelHaege.py
@@ -2,32 +2,51 @@ import numpy as np
 import csv
 import sys
 from copy import copy
+from visualize import *
 
 house_info = {
     #'name':(width, height, extra space)
     'EENGEZINSWONING':(8,8,2),
     'BUNGALOW':(11,7,3),
-    'MAISON':(12, 10,6)
+    'MAISON':(12,10,6)
 }
 
 def place_house(layout, type, x, y):
     w, h, ex = house_info[type]
-    for i in range(ex+1):
-        x1, x2 = x-(ex-i), x+w+(ex-i)
-        y1, y2 = y-i, y+h+i
-        layout[x1:x2, y1:y2] = np.where(layout[x1:x2, y1:y2] != 'W', 'X', 'W')
 
     layout[x:x+w, y:y+h] = type[0]
     houses.append((type, x, y))
 
 def find_spot(layout, houses, type):
     spots = copy(layout)
+    layout_w, layout_h = layout.shape
+    w, h, ex1 = house_info[type]
+
+    spots[0:layout_w, 0:ex1] = np.where(spots[0:layout_w, 0:ex1] == '.', 'X', spots[0:layout_w, 0:ex1])
+    spots[0:ex1, 0:layout_h] = np.where(spots[0:ex1, 0:layout_h] == '.', 'X', spots[0:ex1, 0:layout_h])
+    spots[0:layout_w, layout_h-h-ex1:layout_h] = \
+        np.where(spots[0:layout_w, layout_h-h-ex1:layout_h] == '.', 'X', spots[0:layout_w, layout_h-h-ex1:layout_h])
+    spots[layout_w - w - ex1:layout_w, 0:layout_h] = \
+        np.where(spots[layout_w - w - ex1:layout_w, 0:layout_h] == '.', 'X',spots[layout_w - w - ex1:layout_w, 0:layout_h])
+
     for house in houses:
-        pass
+        type, x, y = house
+        w2, h2, ex2 = house_info[type]
+        ex = max(ex1, ex2) + 1
+        for i in range(ex):
+            x1, x2 = x - w + 1 - i, x + w2 + i
+            y1, y2 = y - h - (ex - i), y + h2 + (ex-i)
+            x1 = max(x1, 0)
+            x2 = max(x2, 0)
+            y1 = max(y1, 0)
+            y2 = max(y2, 0)
+            spots[x1:x2, y1:y2] = np.where(spots[x1:x2, y1:y2] == '.', 'X', spots[x1:x2, y1:y2])
+
+    return spots
 
 layout = np.full([160,180],'.')
 print('Argument List:', str(sys.argv))
-with open('./wijken/wijk_1.csv', newline='') as csvfile:
+with open('./wijken/wijk_3.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, quotechar='"', quoting=csv.QUOTE_ALL,
         skipinitialspace=True)
 
@@ -40,12 +59,23 @@ with open('./wijken/wijk_1.csv', newline='') as csvfile:
             x2, y2 = int(xy2[0]), int(xy2[1])
             layout[x1:x2,y1:y2].fill('W')
         T.append(obj)
-print(layout)
-
 houses = []
 
-place_house(layout, 'EENGEZINSWONING', 4, 4)
-print(layout[:16, :16])
+for i in range(60):
+    free_spots = find_spot(layout, houses, 'EENGEZINSWONING')
+    xcoords, ycoords = np.where(free_spots == '.')
+    if len(xcoords) == 0:
+        print('NO SPACE LEFT AT', i, 'HOUSES!')
+        visualize_map(free_spots)
+        break
+    r = np.random.randint(0, len(xcoords))
+    x, y = xcoords[r], ycoords[r]
+    place_house(layout, 'EENGEZINSWONING', x, y)
+
+free_spots = find_spot(layout, houses, 'EENGEZINSWONING')
+visualize_map(free_spots)
+
+
 
 # struture, bottom_left_xy, top_right_xy, type
 # water_1, "0,0", "32,180", WATER
--- visualize.py
+++ visualize.py
@@ -1,28 +1,48 @@
 import numpy as np
 import pygame
+import random
 
-WIDTH, HEIGHT = (720, 640)
+WIDTH, HEIGHT = (3*160, 3*180)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+GREY = (50, 50, 50)
+BLUE = (0, 0, 255)
+GREEN = (20, 255, 20)
+BROWN = (160, 80, 20)
+RED = (255,0,0)
+
+def draw_grid(screen):
+    for x in range(0, WIDTH, 3):
+        pygame.draw.line(screen, GREY, (x,0), (x,HEIGHT))
+    for y in range(0, HEIGHT, 3):
+        pygame.draw.line(screen, GREY, (0,y), (WIDTH,y))
+
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.DOUBLEBUF)
-pygame.display.set_caption('AmstelHague')
-clock = pygame.time.Clock()
 
-def draw_grid():
-    for x in range(0, WIDTH, 4):
-        pygame.draw.line(screen, BLACK, (x,0), (x,HEIGHT))
-    for y in range(0, HEIGHT, 4):
-        pygame.draw.line(screen, BLACK, (0,y), (WIDTH,y))
 
-in_loop = True
-while in_loop:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit()
+def visualize_map(matrix):
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.DOUBLEBUF)
+    pygame.display.set_caption('AmstelHague')
+    clock = pygame.time.Clock()
 
-    screen.fill(WHITE)
-    draw_grid()
-    pygame.display.flip()
-    clock.tick(30)
+
+    in_loop = True
+    while in_loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+
+        screen.fill(GREEN)
+        for x, row in enumerate(matrix):
+            for y, val in enumerate(row):
+                if val == 'W':
+                    pygame.draw.rect(screen, BLUE, (3*x, 3*y, 3, 3))
+                if val in ['E', 'B', 'M']:
+                    pygame.draw.rect(screen, BROWN, (3*x, 3*y, 3, 3))
+                if val == 'X':
+                    pygame.draw.rect(screen, RED, (3*x, 3*y, 3, 3))
+
+        draw_grid(screen)
+        pygame.display.flip()
+        clock.tick(10)
