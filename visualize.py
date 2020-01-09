bc48e66cedb3c0b6820ebeea94b9e581be37e66d
Michael de Jong <michael.dejong@student.uva.nl>
2020-01-09 11:20:45 +0100

    changed color of the bungalows and changed the loop ending

--- visualize.py
+++ visualize.py
@@ -2,6 +2,7 @@ import numpy as np
 import pygame
 import random
 
+# Variables for the grid and colors
 GRID_W, GRID_H = 3, 3
 
 WIDTH, HEIGHT = (GRID_W*160, GRID_H*180)
@@ -14,6 +15,7 @@ BROWN = (160, 80, 20)
 RED = (255,0,0)
 MARBLE = (230, 230, 200)
 
+# draws a grid on the pygame window
 def draw_grid(screen):
     for x in range(0, WIDTH, GRID_W):
         pygame.draw.line(screen, GREY, (x,0), (x,HEIGHT))
@@ -22,25 +24,28 @@ def draw_grid(screen):
 
 pygame.init()
 
+# uses the matrix to print all water and houses on the screen
 def visualize_map(matrix):
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('AmstelHague')
     clock = pygame.time.Clock()
 
-
+    # main game loop for pygame
     in_loop = True
     while in_loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                quit()
+                in_loop = False
 
         screen.fill(GREEN)
         for x, row in enumerate(matrix):
             for y, val in enumerate(row):
                 if val == 'W':
                     pygame.draw.rect(screen, BLUE, (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
-                if val in ['E', 'B']:
+                if val == 'E':
                     pygame.draw.rect(screen, BROWN, (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
+                if val == 'B':
+                    pygame.draw.rect(screen, BLACK, (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
                 if val in 'M':
                     pygame.draw.rect(screen, MARBLE, (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
                 if val == 'X':
