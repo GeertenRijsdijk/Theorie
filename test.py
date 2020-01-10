2731a9e78b3b7dc9154c739ce0b44d7efe9a94f0
Michael de Jong <michael.dejong@student.uva.nl>
2020-01-10 13:50:55 +0100

    added a test file

---
+++ test.py
@@ -0,0 +1,24 @@
+from algorithms import *
+from copy import deepcopy as d
+filename = './wijken/wijk_1.csv'
+rl = []
+gl = []
+hl = []
+
+c = 20
+layout = load_map(filename)
+counts = [int(c * 0.6), int(c * 0.25), int(c * 0.15)]
+counts[0] += c - sum(counts)
+
+print('Starting')
+for i in range(20):
+    houses = []
+    print('i:',i)
+    rl.append(calculate_price(random(d(layout), c, d(counts))))
+    houses = []
+    gl.append(calculate_price(greedy(d(layout), c, d(counts))))
+    houses = []
+    hl.append(calculate_price(hillclimb(d(layout), c, d(counts))))
+
+print('ded')
+print(np.mean(rl),np.mean(gl),np.mean(hl))
