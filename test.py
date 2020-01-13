b2eb168e2e40793a86e3a6d185087abb2bb44f16
Geerten Rijsdijk <geertenrijsdijk@hotmail.com>
2020-01-13 14:08:22 +0100

    updated test.py

--- test.py
+++ test.py
@@ -5,21 +5,28 @@ rl = []
 gl = []
 hl = []
 
-c = 20
+c = 60
+n_iteration = 10
 layout = load_map(filename)
 counts = [int(c * 0.6), int(c * 0.25), int(c * 0.15)]
 counts[0] += c - sum(counts)
 
 print('Starting')
-for i in range(20):
+for i in range(n_iteration):
     print('ITERATION', i)
     houses *= 0
-    rl.append(calculate_price(random(d(layout), c, d(counts))))
+    _, _, price = random(d(layout), c, d(counts))
+    rl.append(price)
     houses *= 0
-    gl.append(calculate_price(greedy(d(layout), c, d(counts))))
+    _, _, price = greedy(d(layout), c, d(counts))
+    gl.append(price)
     houses *= 0
-    hl.append(calculate_price(hillclimb(d(layout), c, d(counts))))
+    _, _, price = hillclimb(d(layout), c, d(counts))
+    hl.append(price)
 
 print('AVG VALUE WITH RANDOM:     ', np.mean(rl))
+print('MAX VALUE WITH RANDOM:     ', np.max(rl))
 print('AVG VALUE WITH GREEDY:     ', np.mean(gl))
+print('MAX VALUE WITH GREEDY:     ', np.max(gl))
 print('AVG VALUE WITH HILLCLIMBER:', np.mean(hl))
+print('MAX VALUE WITH HILLCLIMBER:', np.max(hl))
