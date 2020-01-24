06326f544c689498b3ae57825d37e7e2b6165c3f
Michael de Jong <michael.dejong@student.uva.nl>
2020-01-24 12:06:57 +0100

    made histogram2 for multiple files, commented visualize more

--- code/visualize.py
+++ code/visualize.py
@@ -6,8 +6,8 @@ Authors:
 - Michael de Jong
 - Geerten Rijsdijk
 
-This file contains a function to visualize the layout of the grid, as well as
-a function that creates a histogram of results in a csv file.
+This file contains a function to visualize the layout of the grid and the price,
+as well as two functions that creates a histograms of results in a csv file.
 '''
 
 import numpy as np
@@ -55,8 +55,6 @@ def draw_grid(screen):
     for y in range(0, HEIGHT, GRID_H):
         pygame.draw.line(screen, GREY, (0,y), (WIDTH,y))
 
-        # Fonts
-
 pygame.init()
 
 # uses the matrix to print all water and houses on the screen
@@ -70,24 +68,31 @@ def visualize_map(grid):
     pygame.display.set_caption('AmstelHague')
     clock = pygame.time.Clock()
 
+    # draw the houses on the screen
     screen.fill(WHITE)
     pygame.draw.rect(screen, GREEN, (0,0,WIDTH,HEIGHT))
     pygame.draw.rect(screen, BLACK, (WIDTH, 0, WIDTH_PLUS - WIDTH, HEIGHT), 1)
     for x, row in enumerate(matrix):
         for y, val in enumerate(row):
             if val == 'W':
-                pygame.draw.rect(screen, BLUE, (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
+                pygame.draw.rect(screen, BLUE,
+                    (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
             if val == 'E':
-                pygame.draw.rect(screen, BROWN, (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
+                pygame.draw.rect(screen, BROWN,
+                    (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
             if val == 'B':
-                pygame.draw.rect(screen, BLACK, (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
+                pygame.draw.rect(screen, BLACK,
+                    (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
             if val in 'M':
-                pygame.draw.rect(screen, MARBLE, (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
+                pygame.draw.rect(screen, MARBLE,
+                    (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
             if val == 'X':
-                pygame.draw.rect(screen, RED, (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
+                pygame.draw.rect(screen, RED,
+                    (GRID_W*x, GRID_H*y, GRID_W, GRID_H))
 
     draw_grid(screen)
 
+    # make the sidebar with information
     if not isinstance(grid, np.ndarray):
         draw_text(screen, 'FILENAME:', 18, WIDTH + 10, 10)
         draw_text(screen, grid.filename, 12, WIDTH + 30, 30)
@@ -111,14 +116,16 @@ def visualize_map(grid):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     location = grid.filename
-                    location = file.strip('.csv')
-                    location = file.replace('/data','/results')
+                    location = location.strip('.csv')
+                    location = location.replace('/data','/results')
                     pygame.image.save(screen, '.' + location + '.png')
 
         clock.tick(60)
 
 # plots a histogram with the values of the csv output files
 def make_histogram(filename, n_houses, algorithm, ran = None):
+
+    # read in the result csv files
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile, quotechar='"', quoting=csv.QUOTE_ALL,
             skipinitialspace=True)
@@ -134,6 +141,7 @@ def make_histogram(filename, n_houses, algorithm, ran = None):
         print('MEAN:', np.mean(val_list))
         print('MAX:', np.max(val_list))
 
+        # round the price values
         for i, v in enumerate(val_list):
             val = round(v, -5)/1000000
             val_list[i] = val
@@ -141,17 +149,63 @@ def make_histogram(filename, n_houses, algorithm, ran = None):
         print('MEAN2:', np.mean(val_list))
         print('MAX2:', np.max(val_list))
 
-        # round the price values and put them into bins
-        if range:
-            bins = [round(ran[0] + 0.1*i, 1) for i in range(int(10*(ran[1]-ran[0]+0.001))+1)]
-            plt.hist(val_list, bins = bins, edgecolor = 'black', rwidth = 0.8, align = 'right')
+        # put the price values into bins and add them to the plot
+        if ran:
+            bins = [round(ran[0] + 0.1*i, 1)
+                for i in range(int(10*(ran[1]-ran[0]+0.001))+1)]
+            plt.hist(val_list, bins = bins, edgecolor = 'black',
+                rwidth = 0.8, align = 'right')
         else:
             min_val = min(val_list)
             max_val = max(val_list)
-            bins = [round(min_val + 0.1*i, 1) for i in range(int(10*(max_val-min_val+0.001))+1)]
-            plt.hist(val_list, bins = bins, edgecolor = 'black', rwidth = 0.8, align = 'right')
+            bins = [round(min_val + 0.1*i, 1)
+                for i in range(int(10*(max_val-min_val+0.001))+1)]
+            plt.hist(val_list, bins = bins, edgecolor = 'black',
+                rwidth = 0.8, align = 'right')
         plt.xlabel('waarde van wijk')
         plt.ylabel('aantal keer')
         plt.show()
 
         return val_list
+
+# As make_histogram but for multiple files.
+def make_histogram2(filenames, n_houses, algorithms):
+    ran = (23,31)
+    val_lists = []
+    if len(filenames) != len(algorithms):
+        return
+    for i in range(len(filenames)):
+        # read in the result csv files
+        with open(filenames[i], newline='') as csvfile:
+            reader = csv.reader(csvfile, quotechar='"',
+                quoting=csv.QUOTE_ALL, skipinitialspace=True)
+            val_list = []
+            for obj in reader:
+                if int(obj[0]) == n_houses and obj[1] == algorithms[i]:
+                    val_list.append(float(obj[-1]))
+
+            if val_list == []:
+                return
+
+            print(len(val_list), 'instances')
+            print('MEAN:', np.mean(val_list))
+            print('MAX:', np.max(val_list))
+
+            # round the price values
+            for i, v in enumerate(val_list):
+                val = round(v, -5)/1000000
+                val_list[i] = val
+
+            print('MEAN2:', np.mean(val_list))
+            print('MAX2:', np.max(val_list))
+
+            # put the price values into bins and add them to the plot
+            bins = [round(ran[0] + 0.1*i, 1)
+                for i in range(int(10*(ran[1]-ran[0]+0.001))+1)]
+            plt.hist(val_list, bins = bins, edgecolor = 'black',
+                rwidth = 0.8, align = 'right')
+
+            val_lists.append(val_list)
+    plt.xlabel('waarde van wijk')
+    plt.ylabel('aantal keer')
+    plt.show()
