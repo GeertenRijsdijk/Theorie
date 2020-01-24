4d561ba53349dd0dba84240d30a3a56854b70cd3
Geerten Rijsdijk <geertenrijsdijk@hotmail.com>
2020-01-24 12:06:34 +0100

    code cleanup

--- results.py
+++ results.py
@@ -27,11 +27,13 @@ import sys
 
 # exit if incorrect number of arguments
 if len(sys.argv) != 5:
-    print("Arguments need to be a path to a file, the amount of houses, the algorithm and the number of iterations")
+    print("Arguments need to be a path to a file, the amount of houses, \
+        the algorithm and the number of iterations")
     sys.exit()
 # exit if not correct algorithm entered
 if sys.argv[3] not in ['r', 'g', 'h','s']:
-    print("Please enter valid algorith: Random = r, Greedy = g, Hillclimb = h, Simann = s")
+    print("Please enter valid algorith: Random = r, Greedy = g, Hillclimb = h \
+        Simann = s")
     sys.exit()
 # calculate the required amount of the different houses
 filename = sys.argv[1]
