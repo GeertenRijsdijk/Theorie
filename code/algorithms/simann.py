d18dffaa95d681ec6ccefd9f4dadf6882c9bfa48
Michael de Jong <michael.dejong@student.uva.nl>
2020-01-27 13:38:25 +0100

    added move and swap functions to limit function size of simann

--- code/algorithms/simann.py
+++ code/algorithms/simann.py
@@ -21,6 +21,50 @@ Returns:
 
 from .random import *
 
+# Function that swaps two houses
+def swap(grid, index, score, T, price_list, type, x, y):
+    # Find random swap
+    ind = np.random.randint(0, len(grid.houses))
+    house_1 = grid.houses[ind]
+    rest_houses = [h for h in grid.houses if h[0] != house_1[0]]
+    ind2 = np.random.randint(0, len(rest_houses))
+    house_2 = rest_houses[ind2]
+
+    new_score = grid.price_after_swap(house_1, house_2)
+
+    # Continue only if the move is legal
+    if new_score > 0:
+        # Calculate probability of swapping
+        p = probability_function(score, new_score, T)
+        if p > np.random.uniform(0,1):
+            # Swap the houses
+            #score = new_score
+            price_list.append(score)
+            grid.swap(house_1, house_2)
+            return new_score
+    return score
+
+# Function that moves a house
+def move(grid, index, score, T, price_list, type, x, y):
+    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    # Choose a random move
+    movex, movey = moves[np.random.randint(0,len(moves))]
+    new_score = grid.calculate_price_of_move(index, movex, movey)
+
+    # Continue only if the move is legal
+    if new_score != 0:
+        # Calculate probability of moving
+        p = probability_function(score, new_score, T)
+        if p > np.random.uniform(0,1):
+            # Move the house
+            # score = new_score
+            price_list.append(score)
+            grid.remove_house(index)
+            grid.place_house(type, x + movex, y + movey, index)
+            return new_score
+    return score
+
 # Function that calculates the probability of moving to a new state
 def probability_function(old_score, new_score, T):
     if new_score > old_score:
@@ -28,10 +72,9 @@ def probability_function(old_score, new_score, T):
     else:
         return np.exp((new_score - old_score) / T)
 
-# simulated annealing algorithm with NO SWAP
+# Simulated annealing algorithm with a chance to swap houses
+# instead of moving them
 def simann(grid, T=10000000, cooling_rate=0.003, stopT=0.1, swap_prob=0.1):
-
-    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
     # initialize a random start state
     random(grid)
@@ -47,37 +90,9 @@ def simann(grid, T=10000000, cooling_rate=0.003, stopT=0.1, swap_prob=0.1):
 
         # Choose whether to move or swap based on swap_prob
         if swap_prob > np.random.uniform(0,1):
-            # Find random swap
-            ind = np.random.randint(0, len(grid.houses))
-            house_1 = grid.houses[ind]
-            rest_houses = [h for h in grid.houses if h[0] != house_1[0]]
-            ind2 = np.random.randint(0, len(rest_houses))
-            house_2 = rest_houses[ind2]
-
-            new_score = grid.price_after_swap(house_1, house_2)
-            # Continue only if the move is legal
-            if new_score > 0:
-                # Calculate probability of swapping
-                p = probability_function(score, new_score, T)
-                if p > np.random.uniform(0,1):
-                    # Swap the houses
-                    score = new_score
-                    price_list.append(score)
-                    grid.swap(house_1, house_2)
+            score = swap(grid, index, score, T, price_list, type, x, y)
         else:
-            # Choose a random move
-            movex, movey = moves[np.random.randint(0,len(moves))]
-            new_score = grid.calculate_price_of_move(index, movex, movey)
-            # Continue only if the move is legal
-            if new_score != 0:
-                # Calculate probability of moving
-                p = probability_function(score, new_score, T)
-                if p > np.random.uniform(0,1):
-                    # Move the house
-                    score = new_score
-                    price_list.append(score)
-                    grid.remove_house(index)
-                    grid.place_house(type, x + movex, y + movey, index)
+            score = move(grid, index, score, T, price_list, type, x, y)
 
         # Cooling rate update
         T *= 1 - cooling_rate
